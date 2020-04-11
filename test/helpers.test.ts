0ca505f468c877e8600a4615835ece448d1d6b22
René Panzar <rene.panzar@dynatrace.com>
2020-04-11 15:43:52 +0200

    Introduce tests for helpers

--- test/helpers.test.ts
+++ test/helpers.test.ts
@@ -1,15 +1,50 @@
-import {toOutputFilename} from "../src";
-import {expect} from "@oclif/test";
+import {toOutputFilename} from '../src';
+import {expect} from '@oclif/test';
 
 describe('helpers', () => {
-  it('toOutputFilename', () => {
-    // given
-    const input = 'a';
+  describe('toOutputFilename', () => {
+    it('should return a.html for input a', () => {
+      // given
+      const input = 'a';
 
-    // when
-    const actual = toOutputFilename(input);
+      // when
+      const actual = toOutputFilename(input);
 
-    // then
-    expect(actual).to.eq('a.html');
+      // then
+      expect(actual).to.eq('a.html');
+    });
+
+    it('should return a.html for input a.md', () => {
+      // given
+      const input = 'a.md';
+
+      // when
+      const actual = toOutputFilename(input);
+
+      // then
+      expect(actual).to.eq('a.html');
+    });
+
+    it('should return a..html for input a.', () => {
+      // given
+      const input = 'a.';
+
+      // when
+      const actual = toOutputFilename(input);
+
+      // then
+      expect(actual).to.eq('a..html');
+    });
+
+    it('should return a.b.html for input a.b.md', () => {
+      // given
+      const input = 'a.b.md';
+
+      // when
+      const actual = toOutputFilename(input);
+
+      // then
+      expect(actual).to.eq('a.b.html');
+    });
   });
 });
